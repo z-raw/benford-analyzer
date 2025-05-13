a5b1d2afdfe7486362a467a860076d1fa41c0912
zrwr <zorawar.sachdev@gmail.com>
2025-05-13 06:46:38 +0200

    Add missing lib for contentNegotiation

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
 
     // Kotlinx Serialization
